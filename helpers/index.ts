4dd38419885f187b391e837e5f9af21346c03d18
Bruno Vercelino da Hora <brunodahora@gmail.com>
2020-02-03 22:38:00 -0300

    Adding first signup page

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,1 +1,18 @@
 export { UserContext } from "./UserContext";
+
+export const addCpfMask = cpf => {
+  if (cpf.length < 3) return cpf;
+
+  const cleanCpf = cpf.replace(/[\.-]/g, "");
+  if (cleanCpf.length < 6)
+    return `${cleanCpf.substring(0, 3)}.${cleanCpf.substring(3)}`;
+  if (cleanCpf.length < 9)
+    return `${cleanCpf.substring(0, 3)}.${cleanCpf.substring(
+      3,
+      6
+    )}.${cleanCpf.substring(6)}`;
+  return `${cleanCpf.substring(0, 3)}.${cleanCpf.substring(
+    3,
+    6
+  )}.${cleanCpf.substring(6, 9)}-${cleanCpf.substring(9)}`;
+};
--- screens/Auth/SignUp/AboutYou.tsx
+++ screens/Auth/SignUp/AboutYou.tsx
@@ -1,10 +1,61 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StatusBar } from "react-native";
+import styled from "styled-components/native";
+import {
+  FullScreenContainer,
+  SolidButton,
+  GradientButton,
+  HeaderText,
+  TextInput
+} from "components";
+import { colors } from "../../../constants";
+import { addCpfMask } from "helpers";
 
-export function AboutYou() {
+const StyledFullScreenContainer = styled(FullScreenContainer)`
+  align-items: flex-start;
+  padding: ${StatusBar.currentHeight + 23}px 16px 32px 16px;
+  width: 100%;
+`;
+
+const StyledHeaderText = styled(HeaderText)`
+  margin: 0 0 60px 5px;
+`;
+
+const FillScreenContainer = styled.View`
+  flex: 1;
+  width: 100%;
+`;
+
+export function AboutYou({ navigation: { navigate } }) {
+  const [name, setName] = React.useState("");
+  const [surname, setSurname] = React.useState("");
+  const [cpf, setCpf] = React.useState("");
+
+  const setCpfWithMask = cpf => setCpf(addCpfMask(cpf));
+
   return (
-    <View>
-      <Text>AboutYou</Text>
-    </View>
+    <StyledFullScreenContainer>
+      <FillScreenContainer>
+        <StyledHeaderText>Sobre Você</StyledHeaderText>
+        <TextInput label="Nome" value={name} onChangeText={setName} />
+        <TextInput
+          label="Sobrenome"
+          value={surname}
+          onChangeText={setSurname}
+        />
+        <TextInput
+          label="CPF"
+          value={cpf}
+          onChangeText={setCpfWithMask}
+          maxLength={14}
+        />
+      </FillScreenContainer>
+      <GradientButton
+        onPress={() => console.log("Cadastrar")}
+        title="Cadastrar"
+        colors={colors.gradient}
+        textColor={colors.white}
+      />
+    </StyledFullScreenContainer>
   );
 }
